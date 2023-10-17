796ef8b17cbe9f1d18e95e9e15068699de658d84
GamerGoat112 <107008636+GamerGoat112@users.noreply.github.com>
2023-10-17 23:50:59 +0000

    working

--- sms.js
+++ sms.js
@@ -5,18 +5,19 @@ const twilioNumber = +18337911819
 
 const client = require('twilio')(accountSid, authToken);
 
+/*
 document.getElementById('submit').addEventListener('click', executeFlow);
-
+*/
 function executeFlow() {
     client.studio.v2.flows('FW5ed3a14fd7b0b1a39c7de8a2cabfcc38')
     .executions
     .create({
         to: document.getElementById('recipientPhoneNumber'),
         from: twilioNumber,
-        parameters: null })
+        parameters: null})
     .then(execution => console.log('Execution created with SID:', execution.sid))
     .then(message => console.log(message.sid))
     .catch(error => console.error('Error creating execution:', error));
 }
 
-    
+executeFlow();
