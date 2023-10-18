7da3eb5aac1b5a19c06e65f6f32d15ee27586e1f
GamerGoat112 <107008636+GamerGoat112@users.noreply.github.com>
2023-10-18 01:17:37 +0000

    jk

--- sms.js
+++ sms.js
@@ -1,23 +1,23 @@
 require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilioNumber = +18337911819
-
+const twilioNumber = process.env.TWILIO_NUMBER_POSTOP;
+const recipientNumber = document.getElementById('recipientPhoneNumber');
 const client = require('twilio')(accountSid, authToken);
 
-/*
-document.getElementById('submit').addEventListener('click', executeFlow);
-*/
+const triggerForm  = document.getElementById('triggerForm');
+
+triggerForm.addEventListener('submit', executeFlow);
+
 function executeFlow() {
     client.studio.v2.flows('FW5ed3a14fd7b0b1a39c7de8a2cabfcc38')
     .executions
     .create({
-        to: document.getElementById('recipientPhoneNumber'),
+        to: recipientNumber,
         from: twilioNumber,
         parameters: null})
     .then(execution => console.log('Execution created with SID:', execution.sid))
     .then(message => console.log(message.sid))
     .catch(error => console.error('Error creating execution:', error));
+    alert('The form was submitted');
 }
-
-executeFlow();
