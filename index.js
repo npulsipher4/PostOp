fc54ec4c5d25c4ea22ae214978208a20ef7e97cf
GamerGoat112 <107008636+GamerGoat112@users.noreply.github.com>
2023-10-25 22:44:54 +0000

    update main

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-
-var recipientNumber = document.getElementById('recipientPhoneNumber');
-
-const triggerForm  = document.getElementById('triggerForm');
-
-triggerForm.addEventListener('submit', postRequest);
-
-function postRequest() {
-    fetch("https://jsonplaceholder.typicode.com/todos", {
-        method: "POST",
-        body: JSON.stringify({
-            phone: document.getElementById('recipientPhoneNumber'),
-            apptDate: '1903-12-17',
-            treatment: 'wt'
-    }),
-    headers: {
-    "Content-type": "application/json; charset=UTF-8"
-    }
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
 triggerForm.addEventListener('submit', executeFlow);
 
 function postRequest() {
@@ -48,3 +26,4 @@ function postRequest() {
     .catch(error => {
         console.error('Error:', error);
     });
+};
