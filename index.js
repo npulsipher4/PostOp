d722ee3c17b9ab0721478bdb24081f69b86019e2
GamerGoat112 <107008636+GamerGoat112@users.noreply.github.com>
2023-10-27 16:34:13 +0000

    update main

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function postRequest() {
     };
 
     // URL of your server endpoint
-    const url = 'http://lyrva.me/users';  // Replace with the actual endpoint URL
+    const url = 'https://postopserver.gamergoat112.repl.co/users';  // Replace with the actual endpoint URL
 
     // Send a POST request
     fetch(url, {
